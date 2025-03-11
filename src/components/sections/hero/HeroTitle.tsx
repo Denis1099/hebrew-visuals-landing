69ad61e7f7f9ed56b934e236fe22b005da0b6c3c
Denis1099 <81426532+Denis1099@users.noreply.github.com>
2025-03-11 13:54:48 +0200

    mobile hero section fix

--- src/components/sections/hero/HeroTitle.tsx
+++ src/components/sections/hero/HeroTitle.tsx
@@ -4,29 +4,29 @@ import { SparklesText } from "@/components/ui/sparkles-text";
 const HeroTitle = () => {
   return (
     <div className="text-center">
-      <h2 className="text-xl sm:text-xl md:text-2xl animate-slide-up leading-relaxed text-[#7105a3] font-assistant hidden md:block">
+      <h2 className="text-xl sm:text-xl md:text-2xl animate-slide-up leading-relaxed text-[#7105a3] font-assistant ">
         אסטרטגיה, צילום ועריכת
       </h2>
-      <h2 className="text-xl sm:text-2xl md:text-4xl font-bold mb-0 md:mb-8 md:mt-0 animate-slide-up leading-relaxed text-[#7105a3] font-assistant hidden md:block">
+      <h2 className="text-2xl sm:text-2xl md:text-4xl font-bold mb-2 md:mb-8 md:mt-0 animate-slide-up leading-relaxed text-[#7105a3] font-assistant">
         וידאו מקצועיים!
       </h2>
       
       <div className="relative mb-4 md:mb-8 flex justify-center">
         <div className="flex items-start">
-          <span className="text-8xl leading-none text-[#c6008a]">"</span>
+          <span className="text-5xl md:text-6xl lg:text-8xl leading-none text-[#c6008a]">"</span>
           <div className="mx-1">
-            <div className="text-4xl sm:text-4xl md:text-5xl lg:text-8xl text-[#c6008a] font-migdal">
+            <div className="text-5xl sm:text-4xl md:text-6xl lg:text-8xl text-[#c6008a] font-migdal">
               אני האמיתי
             </div>
-            <div className="text-4xl sm:text-4xl md:text-5xl lg:text-8xl text-[#004aad] font-migdal">
+            <div className="text-5xl sm:text-4xl md:text-6xl lg:text-8xl text-[#004aad] font-migdal">
               בעסק שלך
             </div>
           </div>
-          <span className="text-8xl leading-none text-[#c6008a]">"</span>
+          <span className="text-5xl md:text-6xl lg:text-8xl leading-none text-[#c6008a]">"</span>
         </div>
       </div>
       
-      <p className="text-lg sm:text-xl md:text-2xl mb-2 animate-slide-up text-[#7105a3] font-assistant">
+      <p className="text-lg sm:text-xl md:text-2xl mb-0 md:mb-2 animate-slide-up text-[#7105a3] font-assistant">
         הגיע הזמן שהעסק שלך יבלוט ויביא תוצאות אמיתיות!
       </p>
       
